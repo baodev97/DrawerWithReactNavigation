8c3236b19efbdea7933bd6a4adf16c10fa66e45d
Le Cong Thien Bao <thienbao@Les-MacBook-Air.local>
2025-12-01 19:45:36 +0700

    configuring the drawer navigator all screen + add icon

--- App.tsx
+++ App.tsx
@@ -1,23 +1,45 @@
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import { NavigationContainer } from '@react-navigation/native';
-import UserScreen from './screen/UserScreen';
-import WelcomeScreen from './screen/WelcomeScreen';
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { NavigationContainer } from "@react-navigation/native";
+import UserScreen from "./screen/UserScreen";
+import WelcomeScreen from "./screen/WelcomeScreen";
 
 const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName='welcome'>
-        <Drawer.Screen name='welcome' component={WelcomeScreen} options={{
-          drawerLabel:"Welcome Screen",
-          headerStyle:{backgroundColor:"#3050d0ff"},
-          headerTintColor:'#ffffff',
-          drawerActiveBackgroundColor:'#ccc',
-          drawerActiveTintColor:'#25398aff',
-          //drawerStyle:{backgroundColor:"#ccc"}
-        }}/>
-        <Drawer.Screen name='user' component={UserScreen}/>
+      <Drawer.Navigator
+        initialRouteName="welcome"
+        screenOptions={{
+          headerStyle: { backgroundColor: "#3050d0ff" },
+          headerTintColor: "#ffffff",
+          drawerActiveBackgroundColor: "#ccc",
+          drawerActiveTintColor: "#25398aff",
+        }}
+      >
+        <Drawer.Screen
+          name="welcome"
+          component={WelcomeScreen}
+          options={{
+            drawerLabel: "Welcome Screen",
+            drawerIcon: ({ color, size }) => (
+              <Ionicons color={color} size={size} name="home" />
+            ),
+            //drawerStyle:{backgroundColor:"#ccc"}
+          }}
+        />
+        <Drawer.Screen
+          name="user"
+          component={UserScreen}
+          options={{
+            drawerLabel: "Welcome Screen",
+            drawerIcon: ({ color, size }) => (
+              <Ionicons color={color} size={size} name="person" />
+            ),
+            //drawerStyle:{backgroundColor:"#ccc"}
+          }}
+        />
       </Drawer.Navigator>
     </NavigationContainer>
   );
